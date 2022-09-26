33f481566535bff4879d70fa77c5c07a4b2ac92b
Lubeck123 <114318419+Lubeck123@users.noreply.github.com>
2022-09-26 22:10:13 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,25 +13,25 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/HwModsWa857'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6285714170944'
+global.youtube = 'https://youtube.com/channel/UC7flTES4TqWBLOPxk25_tsA'
+global.ig = 'https://www.instagram.com/p/CBqPkqsH344/?igshid=YmMyMTA2M2Y='
+global.mygc = 'wa.me/6281227607864'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
-global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
+global.linkgrupss = "https://chat.whatsapp.com/Fj5z7EXvGLVARaZ0cJF8rd"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
+global.email = 'hendralubeck86@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'HENDRA LUBECK' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'NONAME' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'HENDRA' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312']
-global.premium = ['6281214281312']
-global.packname = 'HW MODS WA'
-global.ownerr = ['HW MODS WA']
-global.author = 'HW MODS WA'
+global.owner = ['6281227607864']
+global.premium = ['6281227607864']
+global.packname = 'HENDRA LUBECK'
+global.ownerr = ['NONAME']
+global.author = 'HENDRA LUBECK'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
